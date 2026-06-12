2487a5df66253c51361acd52f08eb4ed06d31e87
Arthur Aibel <Arthur.Aibel1@gmail.com>
2026-06-11 22:24:51 -0300

    Adicionar arquivos de projeto.

---
+++ IndexadorDePalavras/ListaEncadeada/NoListaOcorrencias.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexadorDePalavras.ListaEncadeada
+{
+    internal class NoListaOcorrencias
+    {
+    }
+}
---
+++ IndexadorDePalavras/Models/Ocorrencia.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexadorDePalavras.Models
+{
+    internal class Ocorrencia
+    {
+    }
+}
