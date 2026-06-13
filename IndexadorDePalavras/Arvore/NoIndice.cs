24f64ec8d8cc48cd4cb43d0f3ebc56ac038d770d
Arthur Aibel <Arthur.Aibel1@gmail.com>
2026-06-13 17:35:35 -0300

    Implementação da classe NoIndice

--- IndexadorDePalavras/Arvore/NoIndice.cs
+++ IndexadorDePalavras/Arvore/NoIndice.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IndexadorDePalavras.ListaEncadeada;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,13 @@ namespace IndexadorDePalavras.Arvore
 {
     internal class NoIndice
     {
+        public string Palavra { get; set; }
+        public ListaOcorrencias Lista { get; set; }
+
+        public NoIndice Esquerda { get; set; }
+        public NoIndice Direita { get; set; }
+        public NoIndice Pai { get; set; }
+
+        public bool Vermelho { get; set; }
     }
 }
